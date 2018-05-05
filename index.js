6baa1ad3da47cdf7f69edeae9cb9ebddab78cfe3
Brian <34335880+shidoitsuka@users.noreply.github.com>
2018-05-05 15:01:25 +0700

    Beautified

--- index.js
+++ index.js
@@ -5,23 +5,22 @@ const math = require('mathjs');
 require('./util/eventLoader.js')(bot);
 
 var reload = (message, cmd) => {
-  var msg = message.content.slice(6);
-  if (msg.length < 2) {
-    message.channel.send("Command not found.");
-  }
-  else if (msg.length > 0){
-  delete require.cache[require.resolve('./commands/' + cmd)];
-  try {
-    let cmdFile = require('./commands/' + cmd);
-  } catch (e) {
-    message.channel.send(`Error loading \`${cmd}\`: ${e.name}`).then(
-      response => response.delete(1000).catch(err => console.log(err.stack))
-    ).catch(err => console.log(err.stack));
-  }
-  message.channel.send(`\`${cmd}\` reloaded.`).then(
-    response => response.delete(1000).catch(err => console.log(err.stack))
-  ).catch(err => console.log(err.stack));
-};
+    var msg = message.content.slice(6);
+    if (msg.length < 2) {
+        message.channel.send("Command not found.");
+    } else if (msg.length > 0) {
+        delete require.cache[require.resolve('./commands/' + cmd)];
+        try {
+            let cmdFile = require('./commands/' + cmd);
+        } catch (e) {
+            message.channel.send(`Error loading \`${cmd}\`: ${e.name}`).then(
+                response => response.delete(1000).catch(err => console.log(err.stack))
+            ).catch(err => console.log(err.stack));
+        }
+        message.channel.send(`\`${cmd}\` reloaded.`).then(
+            response => response.delete(1000).catch(err => console.log(err.stack))
+        ).catch(err => console.log(err.stack));
+    };
 }
 
 exports.reload = reload;
