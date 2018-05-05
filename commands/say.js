5addac2c71c77302e3fd47f33e0bb33b67fb2a71
Brian <34335880+shidoitsuka@users.noreply.github.com>
2018-05-05 22:51:25 +0700

    Removed message.delete()
    Created {command}d instead, since its easier to type

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,6 @@
 const config = require('../config.json');
 exports.run = function(bot, message, args) {
-  var args = message.content.split(' ');
-  var msg = args.shift().slice(config.prefix.length);
-  message.delete();
-  message.channel.send(args.join(" "));
+    var args = message.content.split(' ');
+    var msg = args.shift().slice(config.prefix.length);
+    message.channel.send(args.join(" "));
 };
