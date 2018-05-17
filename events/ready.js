16b5bce764362fa32beedf7e49192f2136878c98
Brian <34335880+shidoitsuka@users.noreply.github.com>
2018-05-17 13:59:31 +0700

    Modified

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 module.exports = bot => {
-  console.log(chalk.bgGreen.white("Hello There!"));
+  console.log(chalk.bgGreen.white(`Logged in as ${bot.user.tag}!`));
   bot.user.setStatus("idle");
   bot.user.setActivity('in your room!');
 };
