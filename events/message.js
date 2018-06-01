9d9edb177b5998324da03c92b3be7b53e32660b0
Brian <34335880+shidoitsuka@users.noreply.github.com>
2018-06-01 10:57:19 +0700

    Modified
    * Removed "useless" stuffs

--- events/message.js
+++ events/message.js
@@ -11,60 +11,13 @@ const readdir = promisify(fs.readdir);
 const emojis = ["(#^.^#)", "(⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄", "(✿ꈍ。 ꈍ✿)", "(ؑ‷ᵕؑ̇‷)◞✧", "(灬ºωº灬)♡"];
 const hello = ["H-hello", "H-hi", "M-my prefix is \`m\`", "Miku desu!", "I'm Here!", "Hi!"];
 const badWords = ["fuck", "shit", "bitch"];
-const sucks = ["usak", "usuk", "u sak", "u suk", "usuck"];
-const moms = ["u mom", "ur mom", "your mom", "umom", "urmom", "yourmom"];
 
 // START
 module.exports = async function(message) {
-  // IF INCLUDES OWNER ID
-  if (message.content.includes(config.ownerID)) {
-    if (message.author.bot) return;
-    if (message.content.includes("say")) return;
-    const embed = new Discord.RichEmbed()
-      .setAuthor("Miku")
-      .setColor(0x1a9ca8)
-      .setImage("https://tinyurl.com/DiscordPinged")
-      .setDescription(`Don\'t ping **Brian**, please! UwU`);
-    message.channel.send({
-      embed
-    }).then(response => response.delete(3500));
-    // message.channel.send(`\`${message.author.username}\`, Brian is sleeping right now.\nTry again later.`).then(response => response.delete(10000));
-  }
-  if (message.content.includes(config.babeID)) {
-    if (message.author.bot) return;
-    if (message.content.includes("say")) return;
-    const embed = new Discord.RichEmbed()
-      .setAuthor("Miku")
-      .setColor(0x1a9ca8)
-      .setDescription(`ping me i knock your dick off 1v1 me slut`)
-      .setFooter("-Aere");
-    message.channel.send({
-      embed
-    }).then(response => response.delete(3500));
-  }
-
-  // UR MOM DETECTOR
-  if (moms.some(word =>
-      message.content.toLowerCase().includes(word))) {
-    message.react("🇬");
-    await sleep(500);
-    message.react("🇦");
-    await sleep(1000);
-    message.react("🇾");
-  }
   // BAD WORDS DETECTOR
   if (badWords.some(word =>
       message.content.toLowerCase().includes(word))) {
     message.channel.send(`**${message.author.username}**, Watch your language.`).then(response => response.delete(1000));
-  }
-  // SUCK DETECTOR
-  if (sucks.some(word =>
-      message.content.toLowerCase().includes(word))) {
-    message.react("🇳");
-    await sleep(500);
-    message.react("🇴");
-    await sleep(500);
-    message.react("🇺");
   }
   // MENTION MIKU
   if (message.content.startsWith(`<@${config.botID}>`)) {
