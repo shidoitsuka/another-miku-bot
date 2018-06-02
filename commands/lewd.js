2e60d54ae960a99b737b508134ec8e7a0c0686e4
Brian <34335880+shidoitsuka@users.noreply.github.com>
2018-06-02 08:05:01 +0700

    NSFW Stuff

--- commands/lewd.js
+++ commands/lewd.js
@@ -1,23 +1,106 @@
 const Discord = require('discord.js');
 const lewdLink = require('./lewd.json');
+const nekoclient = require('nekos.life');
+const neko = new nekoclient();
 
-exports.run = (bot, message, args) => {
-  // random link
-  const answers = lewdLink.random();
-  const embed = new Discord.RichEmbed()
-    .setAuthor("Miku -- Lewd", "", answers)
-    .setColor(0x1a9ca8)
-    .setDescription(`Look at ${message.author.username}!\nThey\'re thinking about lewd things! >////<`)
-    .setImage(`${answers}`)
-    .setFooter("Google Images");
-  message.channel.send({
-    embed
-  });
+exports.run = async (bot, message, args) => {
+  if (!args[0]) {
+    const answers = lewdLink.random();
+    const embed = new Discord.RichEmbed()
+      .setAuthor("Miku -- Lewd", "", answers)
+      .setColor(0x1a9ca8)
+      .setDescription(`Look at ${message.author.username}!\nThey\'re thinking about lewd things! >////<`)
+      .setImage(`${answers}`)
+      .setFooter("Google Images");
+    message.channel.send({
+      embed
+    });
+  }
+  if (args[0] == "-pussy") {
+    const answers = await neko.getNSFWPussy();
+    const embed = new Discord.RichEmbed()
+      .setAuthor("Miku -- Lewd", "", `${answers.url}`)
+      .setColor(0x1a9ca8)
+      .setImage(`${answers.url}`);
+    message.channel.send({
+      embed
+    });
+  }
+  if (args[0] == "-random") {
+    const answers = await neko.getNSFWRandomHentaiGif();
+    const embed = new Discord.RichEmbed()
+      .setAuthor("Miku -- Lewd", "", `${answers.url}`)
+      .setColor(0x1a9ca8)
+      .setImage(`${answers.url}`);
+    message.channel.send({
+      embed
+    });
+  }
+  if (args[0] == "-gif-neko") {
+    const answers = await neko.getNSFWNekoGif();
+    const embed = new Discord.RichEmbed()
+      .setAuthor("Miku -- Lewd", "", `${answers.url}`)
+      .setColor(0x1a9ca8)
+      .setImage(`${answers.url}`);
+    message.channel.send({
+      embed
+    });
+  }
+  if (args[0] == "-neko") {
+    const answers = await neko.getNSFWNeko();
+    const embed = new Discord.RichEmbed()
+      .setAuthor("Miku -- Lewd", "", `${answers.url}`)
+      .setColor(0x1a9ca8)
+      .setImage(`${answers.url}`);
+    message.channel.send({
+      embed
+    });
+  }
+  if (args[0] == "-oppai") {
+    const answers = await neko.getNSFWBoobs();
+    const embed = new Discord.RichEmbed()
+      .setAuthor("Miku -- Lewd", "", `${answers.url}`)
+      .setColor(0x1a9ca8)
+      .setImage(`${answers.url}`);
+    message.channel.send({
+      embed
+    });
+  }
+  if (args[0] == "-lesb") {
+    const answers = await neko.getNSFWLesbian();
+    const embed = new Discord.RichEmbed()
+      .setAuthor("Miku -- Lewd", "", `${answers.url}`)
+      .setColor(0x1a9ca8)
+      .setImage(`${answers.url}`);
+    message.channel.send({
+      embed
+    });
+  }
+  if (args[0] == "-cumslut") {
+    const answers = await neko.getNSFWCumsluts();
+    const embed = new Discord.RichEmbed()
+      .setAuthor("Miku -- Lewd", "", `${answers.url}`)
+      .setColor(0x1a9ca8)
+      .setImage(`${answers.url}`);
+    message.channel.send({
+      embed
+    });
+  }
+  if (args[0] == "-anal") {
+    const answers = await neko.getNSFWAnal();
+    const embed = new Discord.RichEmbed()
+      .setAuthor("Miku -- Lewd", "", `${answers.url}`)
+      .setColor(0x1a9ca8)
+      .setImage(`${answers.url}`);
+    message.channel.send({
+      embed
+    });
+  }
   delete require.cache[require.resolve('./lewd.js')];
 };
 
 exports.conf = {
-  aliases: []
+  aliases: ["l"]
 };
 
 exports.help = {
@@ -25,6 +108,6 @@ exports.help = {
   category: "Fun",
   description: ">////< y so lewd!?`",
   usage: "lewd",
-  param: "",
-  aliases: ""
+  param: "-anal\n-pussy\n-random\n-gif-neko\n-neko\n-oppai\n-lesb\n-cumslut",
+  aliases: "l"
 };
