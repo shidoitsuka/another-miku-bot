2b75d895bbf502c52cfe92c566855f40110b744c
Brian <34335880+shidoitsuka@users.noreply.github.com>
2018-06-03 18:23:55 +0700

    More more mo....re?

--- commands/about.js
+++ commands/about.js
@@ -9,11 +9,19 @@ exports.run = (bot, message, args) => {
     .setDescription("Hello, I\'m Miku!\nA \`just4fun\` discord bot written over discord.js framework with ❤ by 12042#5754.")
     .addField("NodeJS version:", `${process.version.slice(1).split(".").join(".")}`, true)
     .addField("discord.js version:", `${package.dependencies["discord.js"].slice(1)}`, true)
-    .addField("Chalk version:", `${package.dependencies["chalk"].slice(1)}`, true)
-    .addField("Enmap version:", `${package.dependencies["enmap"].slice(1)}`, true)
-    .addField("dog-names version:", `${package.dependencies["dog-names"].slice(1)}`, true)
+    .addField("canvas version:", `${package.dependencies["canvas"].slice(1)}`, true)
+    .addField("canvas-constructor version:", `${package.dependencies["canvas-constructor"].slice(1)}`, true)
     .addField("cat-names version:", `${package.dependencies["cat-names"].slice(1)}`, true)
+    .addField("chalk version:", `${package.dependencies["chalk"].slice(1)}`, true)
+    .addField("dog-names version:", `${package.dependencies["dog-names"].slice(1)}`, true)
+    .addField("enmap version:", `${package.dependencies["enmap"].slice(1)}`, true)
+    .addField("enmap-level version:", `${package.dependencies["enmap-level"].slice(1)}`, true)
+    .addField("fs-nextra version:", `${package.dependencies["fs-nextra"].slice(1)}`, true)
+    .addField("gfycat-sdk version:", `${package.dependencies["gfycat-sdk"].slice(1)}`, true)
     .addField("math.js version:", `${package.dependencies["mathjs"].slice(1)}`, true)
+    .addField("nekos.life version:", `${package.dependencies["nekos.life"].slice(1)}`, true)
+    .addField("Invite link:", "[Click Me!](https://discordapp.com/oauth2/authorize/?permissions=2080898303&scope=bot&client_id=364242246314360843)", true)
+
     .setFooter(`Click title for the source-code | v${package.version}-beta`);
   message.channel.send({
     embed
