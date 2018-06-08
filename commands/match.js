dd0ebb0f477205e494468c2bb77e065e675ff5b3
Brian <34335880+shidoitsuka@users.noreply.github.com>
2018-06-08 16:51:48 +0700

    New command(s)
    * More commands, more wuv <3

---
+++ commands/match.js
@@ -0,0 +1,26 @@
+exports.run = (bot, message, args) => {
+  if (!args[1] || same(args)) return message.channel.send(":question:");
+  const splitted = args.join(" ").split("  with  ");
+  const values = splitted.join(" ").trim().split(" with ");
+  if (!args.join(" ").includes("with") || same(values) || args[0] == 'with' || values[2]) return message.channel.send(":question:");
+  const rates = [
+    `1/10 :poop:`, `2/10 :dizzy_face:`, `3/10 :astonished:`, `4/10 :grimacing:`,
+    `5/10 :ok_hand:`,
+    `6/10 :upside_down:`, `7/10 :relieved:`, `8/10 :blush:`, `9/10 :heart:`, `10/10 :heart_eyes:`
+  ].random();
+  message.channel.send(`I\'ll give **${values[0].toLowerCase() == "me" ? "you" : values[0]}** and **${values[1].toLowerCase() == "you" ? "me" : values[1]}** a ${rates}`);
+};
+
+exports.conf = {
+  aliases: ["cocok"],
+  cooldown: 5
+};
+
+exports.help = {
+  name: "match",
+  category: "Fun",
+  description: "Are you going to be a great couple together?",
+  usage: "match \`<x>\` with \`<y>\`",
+  param: "",
+  aliases: "cocok"
+};
---
+++ commands/tag.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const tags = require('./tag.json');
+const fs = require('fs');
+let tagFile = JSON.parse(fs.readFileSync("./commands/tag.json", "utf8"));
+
+exports.run = async (bot, message, args) => {
+  const embed = new Discord.RichEmbed()
+    .setAuthor("Available Tags")
+    .setColor(0xf5ab35)
+    .setDescription(`\`\`\`${Object.keys(tags)}\`\`\``);
+  if (!args[0]) return message.channel.send({
+    embed
+  });
+  if (args[0] == '-add') {
+    if (message.author.id != '332424370272337923') return message.channel.send("Invalid Permission(s).");
+    const tagName = args[1];
+    args.shift();
+    args.shift();
+    const toBeAdded = args.join(" ");
+    tagFile[tagName] = toBeAdded;
+    fs.writeFile('./commands/tag.json', JSON.stringify(tagFile), (err) => {
+      if (err) console.log(err);
+    });
+    return;
+  }
+  try {
+    const sub = args[0];
+    await message.channel.send(tagFile[`${sub}`]);
+  } catch (e) {
+    message.channel.send({
+      embed
+    });
+  }
+};
+
+exports.conf = {
+  aliases: [],
+  cooldown: 5
+};
+
+exports.help = {
+  name: "tag",
+  category: "Util.",
+  description: "",
+  usage: "tag \`<tag_name>\`",
+  param: "",
+  aliases: ""
+};
