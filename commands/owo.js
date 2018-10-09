9389becbb6aa1b173108f50b4cefe433fcfcfe90
Brian <34335880+shidoitsuka@users.noreply.github.com>
2018-10-09 09:45:23 +0700

    Updated to real OωO!

--- commands/owo.js
+++ commands/owo.js
@@ -4,12 +4,12 @@ const neko = new nekoclient();
 
 // START-MAIN
 exports.run = async (bot, message, args) => {
-  if (!args[0]) return message.channel.send(["https://tinyurl.com/MikuOwOHehe", "UwU", "https://tinyurl.com/MikuOwO"].random());
+  if (!args[0]) return message.channel.send(["https://tinyurl.com/MikuOwOHehe", "UωU", "https://tinyurl.com/MikuOwO"].random());
   else {
     const text = await neko.getSFWOwOify({
       text: args.join(" ")
     });
-    message.channel.send(["**Thinking...**", "**Translating...**", "**Twanyswatinyg... OwO**", "**Thinykinyg... OwO**"].random())
+    message.channel.send(["**Thinking...**", "**Translating...**", "**Twanyswatinyg... OωO**", "**Thinykinyg... OωO**"].random())
       .then(m => m.edit(text.owo))
       .then(delete require.cache[require.resolve('./owo.js')]);
   }
