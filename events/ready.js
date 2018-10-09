a88e7c65392630c6f635c14233d5accffd507a22
Brian <34335880+shidoitsuka@users.noreply.github.com>
2018-10-09 09:46:14 +0700

    Auto-change status

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,13 @@
 const chalk = require('chalk');
+const plays = require('../assets/plays.json');
 module.exports = bot => {
   console.log(chalk.bgGreen.white(`Logged in as ${bot.user.tag}!`));
-  bot.user.setStatus("idle");
-  bot.user.setActivity('in your room!');
+  setInterval(() => {
+    let currStat = plays.random();
+    bot.user.setActivity(currStat.activity, {
+      type: plays.type
+    });
+    bot.user.setStatus(currStat.status);
+  }, 90 * 1000);
+  console.log(chalk.bgGreen.white("Booted up!"));
 };
