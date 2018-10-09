0920e8bd078fd2db73d402a9811b8fced3d11284
Brian <34335880+shidoitsuka@users.noreply.github.com>
2018-10-09 09:47:49 +0700

    Added greeting feature!
    Along with the command to set the #channel

---
+++ commands/setwelcome.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+let databaseFile = JSON.parse(fs.readFileSync('./assets/welcome.json', 'utf8'));
+
+exports.run = async (bot, message, args) => {
+  let channelID, guildID = message.guild.id;
+  // if (!message.member.permissions.has("MANAGE_GUILD")) return message.channel.send("You don't have permission to run this!");
+  if (!args[0]) {
+    channelID = message.channel.id;
+    databaseFile[guildID] = channelID
+    fs.writeFile('./assets/welcome.json', JSON.stringify(databaseFile), (err) => {
+      if (err) console.log(err);
+    });
+  }
+  message.channel.send("Aight, I've set the greeting channel!");
+};
+
+exports.conf = {
+  aliases: [],
+  cooldown: 1
+};
+
+exports.help = {
+  name: "setwelcome",
+  category: "Util.",
+  description: "Set a welcome channel.",
+  usage: "setwelcome #[channel-name]",
+  param: "",
+  aliases: ""
+};
--- util/eventLoader.js
+++ util/eventLoader.js
@@ -3,5 +3,6 @@ const bot = new Discord.Client();
 const eventReq = (event) => require(`../events/${event}`);
 module.exports = bot => {
   bot.on('ready', () => eventReq('ready')(bot));
+  bot.on('guildMemberAdd', eventReq('guildMemberAdd'));
   bot.on('message', eventReq('message'));
 };
