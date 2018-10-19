5761fc6e28d29be71932615eb21dbad15bb1a5fe
Brian <34335880+shidoitsuka@users.noreply.github.com>
2018-10-19 09:15:36 +0700

    Some more feature
    Mention someone and then they'll think something xd

--- commands/thonk.js
+++ commands/thonk.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js');
+const embed = new Discord.RichEmbed()
 
 exports.run = (bot, message, args) => {
-  const embed = new Discord.RichEmbed()
-    .setAuthor("Miku")
-    .setColor(0x1a9ca8)
-    .setImage("https://preview.ibb.co/ha8SzS/ohfuck.jpg")
-    .setDescription(`Seems like ${message.author.username} is thinking too much.`);
+  embed.setAuthor("Miku");
+  embed.setColor(0x1a9ca8);
+  embed.setImage("https://preview.ibb.co/ha8SzS/ohfuck.jpg");
+  if (!message.mentions.users.size) embed.setDescription(`Seems like **${message.author.username}** is thinking too much.`);
+  else embed.setDescription(`Seems like **${message.mentions.users.first().username}** is thinking too much.`);
   message.channel.send({
     embed
   });
