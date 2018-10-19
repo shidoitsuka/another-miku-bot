a13bf327b04b102733570bd2a88a11cd0e756cde
Brian <34335880+shidoitsuka@users.noreply.github.com>
2018-10-19 09:29:25 +0700

    Updated

--- commands/eval.js
+++ commands/eval.js
@@ -1,6 +1,6 @@
 const util = require('util');
 exports.run = async (bot, message, args) => {
-  // if (message.author.id != "332424370272337923" || !args[0]) return message.channel.send(":question:");
+  if (message.author.id != "332424370272337923" || !args[0]) return message.channel.send(":question:");
   const code = args.join(" ");
   if (typeof code !== 'string') code = util.inspect(code, {
     depth: 0
@@ -12,11 +12,15 @@ exports.run = async (bot, message, args) => {
     coded = eval(code);
     reply.edit(coded, {
       code: 'js'
-    });
+    }).catch(e => reply.edit(e.stack, {
+      code: 'js'
+    }));;
   } catch (e) {
     reply.edit(e.stack, {
       code: 'js'
-    });
+    }).catch(e => reply.edit(e.stack, {
+      code: 'js'
+    }));;
   }
 };
 
