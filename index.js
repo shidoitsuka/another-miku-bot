40e7a7df14c6ff5302e07e2f6b461441cff2c62c
Brian <34335880+shidoitsuka@users.noreply.github.com>
2018-11-12 10:20:08 +0700

    Removed all unused stuff(s)
    and buggy thingies

--- index.js
+++ index.js
@@ -8,24 +8,6 @@ var talkedRecently = JSON.parse(fs.readFileSync('./assets/cooldowns.json', 'utf8
 require('./util/eventLoader.js')(bot);
 require('./modules/function.js')(bot);
 
-// RELOAD
-var reload = (message, cmd) => {
-  var msg = message.content.slice(6);
-  if (msg.length < 2) {
-    message.channel.send("Please includes the command\'s name.");
-  } else if (msg.length > 0) {
-    delete require.cache[require.resolve('./commands/' + cmd)];
-    try {
-      let cmdFile = require('./commands/' + cmd);
-    } catch (e) {
-      message.channel.send(`Error loading \`${cmd}\`: ${e.name}`).catch(err => console.log(err.stack));
-    }
-    message.channel.send(`\`${cmd}\` reloaded.`).then(
-      response => response.delete(1000).catch(err => console.log(err.stack))
-    ).catch(err => console.log(err.stack));
-  };
-}
-
 bot.config = require('./config.json');
 bot.commands = new Enmap();
 bot.aliases = new Enmap();
@@ -35,7 +17,7 @@ bot.cdTime = new Enmap();
 const init = async () => {
   const folder = walker(`./commands/`)
     .on('file', (file) => {
-      // if (!f.endsWith('.js')) return;
+      if (!file.endsWith('.js')) return;
       let response = bot.loadCommand(file);
       if (response) console.log(response);
     });
@@ -47,6 +29,5 @@ fs.writeFile('./assets/cooldowns.json', JSON.stringify(talkedRecently), (err) =>
   if (err) console.log(err);
 });
 
-exports.reload = reload;
 init();
 bot.login(bot.config.token);
