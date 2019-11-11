306ed4fce4ca33519ca166d74c0785524702aa25
shidoitsuka <shidoitsukaa@gmail.com>
2019-11-11 10:53:14 +0700

    added new events loader

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -4,6 +4,8 @@ const eventReq = (event) => require(`../events/${event}`);
 module.exports = bot => {
   bot.on('ready', () => eventReq('ready')(bot));
   bot.on('guildMemberAdd', eventReq('guildMemberAdd'));
+  bot.on('guildCreate', eventReq('guildCreate'));
+  bot.on('guildDelete', eventReq('guildDelete'));
   bot.on('messageReactionAdd', eventReq('messageReactionAdd'));
   bot.on('message', eventReq('message'));
 };
