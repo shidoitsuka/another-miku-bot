8bce4d85cd1f7d1a28a39f28170176dd074b8209
shidoitsuka <shidoitsukaa@gmail.com>
2019-12-13 02:49:04 +0700

    [2.0-pre] updated
    - removed unused package(s)
    - removed json based database

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,15 +1,16 @@
 const Discord = require("discord.js");
-const fs = require("fs");
 
 module.exports = member => {
-  const DB = readFile("./assets/guildDB");
-  if (DB[member.guild.id].greetingChannel == "") return;
+  const bot = member.client;
+  // prettier-ignore
+  if (bot.db.get("guildConf", `${member.guild.id}.greetingChannel`) == null) return;
   const embed = new Discord.MessageEmbed()
     .setAuthor("New Member!")
     .setDescription(`**${member.user.username}** just joined!`)
     .setImage("https://media.giphy.com/media/b9QBHfcNpvqDK/giphy.gif")
     .setColor("0xf5ab35");
+  // prettier-ignore
   member.guild.channels
-    .find(c => c.id === DB[member.guild.id].greetingChannel)
+    .find(c => c.id === bot.db.get("guildConf", `${member.guild.id}.greetingChannel`))
     .send({ embed });
 };
