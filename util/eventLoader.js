be5bf67c0229f43771cc1a529a255f9cfe883f5b
shidoitsuka <shidoitsukaa@gmail.com>
2019-12-13 02:43:42 +0700

    beautified

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,11 +1,9 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-const eventReq = (event) => require(`../events/${event}`);
+const eventReq = event => require(`../events/${event}`);
 module.exports = bot => {
-  bot.on('ready', () => eventReq('ready')(bot));
-  bot.on('guildMemberAdd', eventReq('guildMemberAdd'));
-  bot.on('guildCreate', eventReq('guildCreate'));
-  bot.on('guildDelete', eventReq('guildDelete'));
-  bot.on('messageReactionAdd', eventReq('messageReactionAdd'));
-  bot.on('message', eventReq('message'));
+  bot.on("ready", () => eventReq("ready")(bot));
+  bot.on("guildMemberAdd", eventReq("guildMemberAdd"));
+  bot.on("guildCreate", eventReq("guildCreate"));
+  bot.on("guildDelete", eventReq("guildDelete"));
+  bot.on("messageReactionAdd", eventReq("messageReactionAdd"));
+  bot.on("message", eventReq("message"));
 };
