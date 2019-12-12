0a54e5a66ea145b7633462d4e51a11037aad4251
shidoitsuka <shidoitsukaa@gmail.com>
2019-12-13 02:49:16 +0700

    [2.0-pre] updated
    - removed json based database

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -2,9 +2,11 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports = guild => {
-  let DB = readFile("./assets/guildDB");
-
-  if (DB[guild.id] == undefined) return;
-  delete DB[guild.id];
-  writeFile("./assets/guildDB", DB);
+  const bot = guild.client;
+  try {
+    const checker = bot.db.get("guildConf", guild.id);
+    if (checker != undefined) throw Error();
+  } catch (_) {
+    bot.db.deleteProp("guildConf", guild.id);
+  }
 };
