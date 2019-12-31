e1a0e564cea80c83e4cfe90ed7d894297f3afa58
shidoitsuka <mail@standinshd.site>
2019-12-31 14:51:03 +0700

    fixed miku status not showing

--- events/ready.js
+++ events/ready.js
@@ -5,8 +5,9 @@ module.exports = bot => {
   console.log(chalk.bgGreen.white(`Logged in as ${bot.user.tag}!`));
   setInterval(() => {
     let currStat = plays.random();
-    bot.user.setActivity(currStat.activity, { type: plays.type });
-    bot.user.setStatus(currStat.status);
+    // prettier-ignore
+    bot.user.setPresence({ activity: { name: currStat.activity }, status: plays.status  })
   }, 90 * 1000);
+
   console.log(chalk.bgGreen.white("Booted up!"));
 };
