ae6067c2f48c96efb0bcad29b23666e87a193841
shidoitsuka <mail@standinshd.site>
2020-08-27 18:13:31 +0000

    updated

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   "prefix": "q",
   "botID": "364242246314360843",
   "ownerID": "332424370272337923",
-  "token": "MzY0MjQyMjQ2MzE0MzYwODQz.Xr02YA.PbEImqKRWdInb1lwpnswQOXJP4w",
+  "token": "MzY0MjQyMjQ2MzE0MzYwODQz.WdGpBg.AYfNb_u9DxPd00V2rTUr-bRkya0",
   "googleid": "015304749108048351151:0qyepo8csv4",
   "googleapi": "AIzaSyACOOxQ-jEWA7BUSBSFfxqMe2tLsDP1_H8",
   "gfycatid": "2_q6HtBE",
